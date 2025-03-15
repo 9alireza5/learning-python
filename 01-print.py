a35f3b7cc90209775c57a18b3a5741458accf0e2
alireza <alireza.vb1995@gmail.com>
2025-03-15 13:35:20 +0330

    Add files via upload

---
+++ 01-print.py
@@ -0,0 +1,10 @@
+print ('hello world!!!')
+print('^_^')
+a=4
+b=5
+name ='alireza'
+age=29
+job='MSc student of structural engineering at university of tehran'
+print ('my name',name,'and i am a',job,'and i am',age,'years old')
+print('----------')
+print('a is equal to ',a,'and b is equal to',b,'and sum of',a,'and',b,'is equal to',a+b)
