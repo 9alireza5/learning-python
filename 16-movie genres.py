ce32fd12630607675cfd559a06ab8cb8f493e540
alireza <alireza.vb1995@gmail.com>
2025-05-12 23:14:15 +0330

    16-movie genres.py

--- 16-movie genres.py
+++ 16-movie genres.py
@@ -1,21 +1,16 @@
 numin = int(input())
-collection = dict()
-
-for i in range(numin):
-    parts = input().split()
-    key = parts[0]
-    value = parts[1:]
-    collection[key] = value
-
 genre_list = ["Horror", "Romance", "Comedy", "History", "Adventure", "Action"]
 genre_counter = {genre: 0 for genre in genre_list}
 
-for genres in collection.values():
+for i in range(numin):
+    parts = input().split()
+    genres = parts[1:]
+
     for g in genres:
         if g in genre_counter:
             genre_counter[g] += 1
+
 sorted_output = sorted(genre_counter.items(), key=lambda x: (-x[1], x[0]))
 
 for genre, count in sorted_output:
     print(f"{genre} : {count}")
-
